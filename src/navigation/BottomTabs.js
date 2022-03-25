90b75729556f31ba1e9cf358123336d5b53a1f5c
Richard Haddad <richard@haddads.net.au>
2022-03-25 23:35:19 +1000

    Fixed directory attribution for PHPStorm

--- src/navigation/BottomTabs.js
+++ src/navigation/BottomTabs.js
@@ -1,9 +1,24 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+// Import Screens
+import HomeScreen from "../screens/HomeScreen";
+import SearchScreen from "../screens/SearchScreen";
+import PostNewScreen from "../screens/PostNewScreen";
+import MessagesScreen from "../screens/MessagesScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+
 const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
     return (
-
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Search" component={SearchScreen} />
+            <Tab.Screen name="New Post" component={PostNewScreen} />
+            <Tab.Screen name="Messages" component={MessagesScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
     );
 };
+
+export default BottomTabs;
