5a0c34513eaba1998bbf241550332d4e89a50b62
Richard Haddad <richard@haddads.net.au>
2022-04-02 06:53:48 +1000

    Paper theme minor config changes

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -3,6 +3,20 @@ import { OSTRACON_ACCENT, OSTRACON_BADGE, OSTRACON_PRIMARY } from "./Colours";
 
 export const OstraconTheme = {
   ...DefaultTheme,
+  dark: false,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: OSTRACON_PRIMARY,
+    accent: OSTRACON_ACCENT,
+    notification: OSTRACON_BADGE,
+  },
+};
+
+export const OstraconDarkTheme = {
+  ...DefaultTheme,
+  dark: true,
+  mode: "exact",
   roundness: 2,
   colors: {
     ...DefaultTheme.colors,
