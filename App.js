e56b0c8fba3141ce4311023d7ed2e90104ec4500
Richard Haddad <richard@haddads.net.au>
2022-04-02 07:38:07 +1000

    Set up dark/light mode with context

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
-import { Provider as PaperProvider } from "react-native-paper";
-import { OstraconTheme } from "./src/theme/Theme";
 import { AppRegistry, Platform } from "react-native";
 
+// Import Theme Libraries
+import {
+  Provider as PaperProvider,
+  DefaultTheme as PaperDefaultTheme,
+  DarkTheme as PaperDarkTheme,
+} from "react-native-paper";
+import { OstraconTheme } from "./src/theme/Theme";
+import { ThemeContext } from "./src/context/ThemeContext";
+import deepmerge from "deepmerge";
+
 // Import Navigation Libraries
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DefaultTheme as NavigateDefaultTheme,
+  DarkTheme as NavigateDarkTheme,
+} from "@react-navigation/native";
 
 // Import Custom Navigation
 import { navigationRef } from "./src/navigation/RootNavigation";
@@ -17,13 +29,35 @@ if (Platform.OS === "android") {
   AppRegistry.registerComponent("main", () => App);
 }
 
+const OstraconDefaultTheme = deepmerge(PaperDefaultTheme, NavigateDefaultTheme);
+const OstraconDarkTheme = deepmerge(PaperDarkTheme, NavigateDarkTheme);
+
 const App = () => {
+  // States
+  const [isThemeDark, setIsThemeDark] = useState(false);
+
+  let theme = isThemeDark ? OstraconDarkTheme : OstraconDefaultTheme;
+
+  const toggleTheme = React.useCallback(() => {
+    return setIsThemeDark(!isThemeDark);
+  }, [isThemeDark]);
+
+  const preferences = React.useMemo(
+    () => ({
+      toggleTheme,
+      isThemeDark,
+    }),
+    [toggleTheme, isThemeDark]
+  );
+
   return (
-    <PaperProvider theme={OstraconTheme}>
-      <NavigationContainer ref={navigationRef}>
-        <StacksNavigator />
-      </NavigationContainer>
-    </PaperProvider>
+    <ThemeContext.Provider value={preferences}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer theme={theme} ref={navigationRef}>
+          <StacksNavigator />
+        </NavigationContainer>
+      </PaperProvider>
+    </ThemeContext.Provider>
   );
 };
 
