8643ae9bbac78aeac88427eb33a00d38ce8e9f19
Richard Haddad <richard@haddads.net.au>
2022-04-27 21:23:08 +1000

    List padding corrected

---
+++ src/components/ListFooter.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+import { BOTTOM_NAVIGATION_FULL_HEIGHT } from "../constants/constants";
+
+const ListFooter = () => {
+  return <View style={{ height: BOTTOM_NAVIGATION_FULL_HEIGHT }} />;
+};
+
+export default ListFooter;
