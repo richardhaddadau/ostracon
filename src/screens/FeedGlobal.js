35c39232413f1f91ef815f6d5e14eec38320f329
Richard H <richard@haddads.net.au>
2022-05-05 04:07:44 +1000

    Adjusting header title by tab screen

--- src/screens/FeedGlobal.js
+++ src/screens/FeedGlobal.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text } from "react-native";
+import CustomHeader from "../navigation/Member/CustomHeader";
+import { useFocusEffect } from "@react-navigation/native";
 
-const FeedGlobal = ({ navigation }) => {
+const FeedGlobal = ({ navigation, setScreen }) => {
+  useFocusEffect(() => {
+    setScreen("Global Feed");
+  });
+
   return (
     <View>
       <Text>Global</Text>
