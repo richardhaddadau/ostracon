0d2dfef84c34e97fff42b8923de4c3475aab2c28
Richard Haddad <richard@haddads.net.au>
2022-05-26 19:58:17 +1000

    Removed MMKV. Installed Expo Secure Store

--- src/utils/AsyncOps.js
+++ src/utils/AsyncOps.js
@@ -1,7 +1,4 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-// import { MMKV } from "react-native-mmkv";
-
-// export const storage = new MMKV();
+import * as SecureStore from "expo-secure-store";
 
 // Process Next Screen
 const SignUpNext = (screen) => {
@@ -12,53 +9,25 @@ const SignUpNext = (screen) => {
     4: ["location"],
   };
 
-  // const currentData = getMMKV("signup");
+  const currentData = getSecureStore("signup");
 
   console.log(`Screen: ${screen}`);
-  console.log(getRegistrationData());
+  // console.log(getRegistrationData());
 };
 
-// MMKV Procedures
-// const getMMKV = (key) => {
-//   const strFrom = storage.getString(key);
-//   const objFrom = JSON.parse(strFrom);
-//
-//   return objFrom;
-// };
-//
-// const setMMKV = (key, valueObj) => {
-//   if (!key || !valueObj) return undefined;
-//
-//   const strTo = JSON.stringify(valueObj);
-//   storage.set(key, strTo);
-//
-//   return true;
-// };
-
-// AsyncStorage Procedures
-const getRegistrationData = async () => {
-  try {
-    const registrationObject = AsyncStorage.getItem(
-      "@current_registration_details"
-    );
-
-    return JSON.parse(await registrationObject);
-  } catch (e) {
-    // Error Reading
-  }
+// Expo Secure Store
+const getSecureStore = async (key) => {
+  const strFrom = await SecureStore.getItemAsync(key);
+  return JSON.parse(strFrom);
 };
 
-const saveRegistrationData = async (registrationObject) => {
-  try {
-    await AsyncStorage.clear();
-    const registrationString = JSON.stringify(registrationObject);
-    await AsyncStorage.setItem(
-      "@current_registration_details",
-      registrationString
-    );
-  } catch (e) {
-    // store nothing
-  }
+const setSecureStore = async (key, valueObj) => {
+  if (!key || !valueObj) return undefined;
+
+  const strTo = JSON.stringify(valueObj);
+  await SecureStore.setItemAsync(key, strTo);
+
+  return true;
 };
 
-export { SignUpNext, getRegistrationData, saveRegistrationData };
+export { SignUpNext, getSecureStore, setSecureStore };
