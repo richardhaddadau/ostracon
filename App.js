b79d93a042379f50c5cf96b8bfe307542523f326
Richard Haddad <richard@haddads.net.au>
2022-06-11 05:51:57 +1000

    Removed starting console.log

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React, { useState, useContext, useRef, useEffect } from "react";
 import { View, StatusBar } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { SafeAreaProvider } from "react-native-safe-area-context/src/SafeAreaContext";
-import * as Localization from "expo-localization";
 import checkForIdle from "./src/services/checkForIdle";
 
 // Import Theme Libraries
@@ -51,40 +50,6 @@ export const App = () => {
     setTheme(nextTheme);
     setMyTheme(myNextTheme);
   };
-
-  useEffect(() => {
-    const userTimezone = Localization.timezone;
-
-    // console.log(userTimezone);
-
-    moment.locale("en-au");
-
-    // const thisDate =
-    //   Platform.OS === "ios"
-    //     ? `iOS: ${new Date("2022-05-23T14:17:33.430Z").toLocaleDateString(
-    //         "en-gb",
-    //         {
-    //           year: "numeric",
-    //           month: "long",
-    //           day: "2-digit",
-    //           timeZone: userTimezone,
-    //         }
-    //       )}`
-    //     : `Android: ${moment("2022-05-23T14:17:33.430Z").format(
-    //         "D MMMM YYYY"
-    //       )}`;
-
-    const thisDate = moment("2022-05-23T14:17:33.430Z").format("D MMMM YYYY");
-
-    console.log(`This: ${thisDate}`);
-
-    // console.log(new Date("1987-09-17"));
-
-    // Epoch the TS from the Document creation time
-    // ToString the Epoch to return the time in String format
-    // Use Expo Localization to get TimeZone
-    // Use new Date() to set the TimeZone using EN-GB time
-  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
