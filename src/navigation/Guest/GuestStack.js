af5cccf6bfc535ce3f582ef6d20978aa5140ae87
Richard Haddad <richard@haddads.net.au>
2022-06-11 05:44:26 +1000

    OnBoarding Screens added to Stack

--- src/navigation/Guest/GuestStack.js
+++ src/navigation/Guest/GuestStack.js
@@ -8,6 +8,10 @@ import WelcomeScreen from "../../screens/WelcomeScreen";
 import SignInScreen from "../../screens/SignInScreen";
 import SignUpScreen from "../../screens/SignUpScreen";
 import SignOutScreen from "../../screens/SignOutScreen";
+import OnBoardingOneScreen from "../../screens/OnBoardingOneScreen";
+import OnBoardingTwoScreen from "../../screens/OnBoardingTwoScreen";
+import OnBoardingThreeScreen from "../../screens/OnBoardingThreeScreen";
+import OnBoardingFourScreen from "../../screens/OnBoardingFourScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +19,22 @@ const GuestStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="OnBoardingOneScreen"
+        component={OnBoardingOneScreen}
+      />
+      <Stack.Screen
+        name="OnBoardingTwoScreen"
+        component={OnBoardingTwoScreen}
+      />
+      <Stack.Screen
+        name="OnBoardingThreeScreen"
+        component={OnBoardingThreeScreen}
+      />
+      <Stack.Screen
+        name="OnBoardingFourScreen"
+        component={OnBoardingFourScreen}
+      />
       <Stack.Screen name="Login" component={SignInScreen} />
       <Stack.Screen name="Register" component={SignUpScreen} />
       <Stack.Screen name="Log Out" component={SignOutScreen} />
