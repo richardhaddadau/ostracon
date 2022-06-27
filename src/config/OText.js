63ccc50feb15655bc8f28fcf2920dd3884a4331a
Richard Haddad <richard@haddads.net.au>
2022-06-28 04:29:20 +1000

    OText progress

--- src/config/OText.js
+++ src/config/OText.js
@@ -2,7 +2,15 @@ import React from "react";
 import { Text } from "react-native";
 
 const OText = (props) => {
+  const processedText = [];
   const processText = (text, action, colour) => {
     let tagList = props.children.match(/[@#][a-z0-9_\.]+/gi);
+    if (tagList == null) {
+      return [text];
+    }
+
+    for (const tag of tagList) {
+      processedText.push(text.substring(0, text.indexOf(tag)));
+    }
   };
 };
