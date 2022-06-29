5fda21913267360115ff0b77bf8eaab739ee4cb4
Richard H <richard@haddads.net.au>
2022-06-29 21:37:49 +1000

    Minor cleanup

--- src/config/OText.js
+++ src/config/OText.js
@@ -4,11 +4,15 @@ import { Text } from "react-native";
 const OText = (props) => {
   const processedText = [];
   const processText = (text, action, colour) => {
+    // match all mentions and hashtags in text
     let tagList = props.children.match(/[@#][a-z0-9_\.]+/gi);
+
+    // If text has no mentions or hashtags, return as is
     if (tagList == null) {
       return [text];
     }
 
+    // add each mention and hashtag to our array
     for (const tag of tagList) {
       processedText.push(text.substring(0, text.indexOf(tag)));
     }
