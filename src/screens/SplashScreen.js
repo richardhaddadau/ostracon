ede27854452e1298c38c242798f81420c9959322
Richard H <richard@haddads.net.au>
2022-07-31 00:02:15 +1000

    Crated splash screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,5 +1,44 @@
 import React from "react";
+import { ActivityIndicator } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context/src/SafeAreaContext";
 
-const SplashScreen = () => {};
+// Import custom libraries
+import CustomStatusBar from "../components/CustomStatusBar";
+import { Image } from "react-native";
+import { useTheme } from "@ui-kitten/components";
+import { TYPICAL_MARGIN } from "../constants/constants";
+
+const SplashScreen = () => {
+  // Theme
+  const theme = useTheme();
+
+  return (
+    <SafeAreaProvider
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CustomStatusBar backgroundColor={"#363744"} barStyle={"light-content"} />
+      <Image
+        source={require("../../assets/Ostracon-Logo.png")}
+        style={{
+          marginBottom: TYPICAL_MARGIN,
+
+          width: 170,
+          height: 60,
+
+          resizeMode: "contain",
+        }}
+      />
+
+      <ActivityIndicator
+        size={"large"}
+        color={theme["color-secondary-default"]}
+      />
+    </SafeAreaProvider>
+  );
+};
 
 export default SplashScreen;
